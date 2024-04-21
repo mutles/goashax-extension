313034b2270940e74a9c7639b4fb9e1fa9a8d4eb
Mutles <hinmocha@gmail.com>
2024-04-21 18:58:17 +0200

    remove old

--- src/goashax-opener.js
+++ src/goashax-opener.js
@@ -1,92 +1,58 @@
 
 function openTab(username) {
-    chrome.tabs.create({ url: `https://www.twitch.tv/${username}`, active: false }, (tab) => {
-        chrome.tabs.update(tab.id, { highlighted: false, muted: true });
+    browser.tabs.create({ url: `https://www.twitch.tv/${username}`, active: false }, (tab) => {
+        browser.tabs.update(tab.id, { highlighted: false, muted: true });
     });
 }
 
-function sleep(duration) {
-    let cancelTimer;
-    const promise = new Promise((resolve, reject) => {
-        const timeoutID = setTimeout(resolve, duration);
-        cancelTimer = () => {
-            clearTimeout(timeoutID);
-            resolve();
-        };
-    });
-    return [promise, cancelTimer];
+async function getIsActive() {
+    return await browser.storage.local.get("ghax_enabled");
 }
 
-function getStorage(name) {
-    return new Promise((resolve, reject) => chrome.storage.local.get(name, (x) => resolve(x[name])));
+async function getInterval() {
+    let interval = await browser.storage.local.get("interval");
+    return Math.max(interval ? interval : 5, 1);
 }
 
-class Listener {
-    constructor() {
-        this.sleepDuration;
-        this.username = "goashaxtv";
-        this.previouslyOnline = false;
+async function getWasPreviouslyOnline() {
+    return await browser.storage.local.get("previously_online");
+}
 
-        this.toShutdown = false;
-        this.promise;
-        this.cancel;
-        this.isActive = false;
-    }
-
-    async stop() {
-        this.toShutdown = true;
-        if (this.cancel) {
-            this.cancel();
-            await this.promise;
-        }
-    }
-
-    async start() {
-        this.toShutdown = false;
-        await this.init();
-        if (this.isActive) {
-            this.listen();
-        }
-    }
-
-    async init() {
-        let interval = await getStorage("interval");
-        this.sleepDuration = Math.max(interval ? interval : 5, 1) * 60 * 1000;
-        this.isActive = await getStorage("ghax_enabled");
-        console.log(this.isActive, this.sleepDuration);
-    }
-
-    async reload() {
-        await this.stop();
-        this.start();
-    }
-
-    async listen() {
-        while (!this.toShutdown) {
-            let response = await fetch(`https://decapi.me/twitch/uptime/${this.username}`);
-            let text = await response.text();
-            let isOnline = !text.includes("offline");
-            if (isOnline && !this.previouslyOnline) {
-                openTab(this.username);
+async function configChanged() {
+    browser.alarms.clearAll();
+    if (await getIsActive()) {
+        browser.alarms.create(
+            "check_online",
+            {
+                delayInMinutes: 0,
+                periodInMinutes: await getInterval(),
             }
-            this.previouslyOnline = isOnline;
-            [this.promise, this.cancel] = sleep(this.sleepDuration);
-            await this.promise;
-        }
+        )
     }
 }
 
-async function init() {
-    let listener = new Listener();
-    listener.start();
-    chrome.runtime.onMessage.addListener(function(request, sender) {
-        if (request.type === "config_changed") {
-            listener.reload();
-            return Promise.resolve();
-        }
-        return false;
-    });
+async function checkOnline() {
+    let response = await fetch(`https://decapi.me/twitch/uptime/${this.username}`);
+    let text = await response.text();
+    let isOnline = !text.includes("offline");
+    let wasPreviouslyOnline = await getWasPreviouslyOnline();
+    if (isOnline && !wasPreviouslyOnline) {
+        openTab(this.username);
+    }
+    await browser.storage.local.set("previously_online", isOnline);
 }
 
-init();
+browser.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === "check_online") {
+        return checkOnline();
+    }
+    return false;
+});
+
+browser.runtime.onMessage.addListener(function(request, sender) {
+    if (request.type === "config_changed") {
+        return configChanged();
+    }
+    return false;
+});
 
