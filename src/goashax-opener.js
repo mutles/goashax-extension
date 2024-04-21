d12f416670d53004e82adedf77d14612266867f9
Mutles <hinmocha@gmail.com>
2024-04-21 21:33:29 +0200

    Update

--- src/goashax-opener.js
+++ src/goashax-opener.js
@@ -5,41 +5,59 @@ function openTab(username) {
     });
 }
 
+async function getStorage(name) {
+    return (await browser.storage.local.get(name))[name]
+}
+
 async function getIsActive() {
-    return await browser.storage.local.get("ghax_enabled");
+    return await getStorage("ghax_enabled");
 }
 
 async function getInterval() {
-    let interval = await browser.storage.local.get("interval");
+    let interval = await getStorage("interval");
     return Math.max(interval ? interval : 5, 1);
 }
 
 async function getWasPreviouslyOnline() {
-    return await browser.storage.local.get("previously_online");
+    return await getStorage("previously_online") || false;
+}
+
+async function getUsername() {
+    return "goashaxtv";
 }
 
 async function configChanged() {
     browser.alarms.clearAll();
     if (await getIsActive()) {
+        let interval = await getInterval();
         browser.alarms.create(
             "check_online",
             {
-                delayInMinutes: 0,
-                periodInMinutes: await getInterval(),
+                delayInMinutes: interval,
+                periodInMinutes: interval,
             }
         )
+        await checkOnline();
     }
 }
 
 async function checkOnline() {
-    let response = await fetch(`https://decapi.me/twitch/uptime/${this.username}`);
+    console.log("Checking online status");
+    let username = await getUsername();
+    let response = await fetch(`https://decapi.me/twitch/uptime/${username}`);
     let text = await response.text();
     let isOnline = !text.includes("offline");
     let wasPreviouslyOnline = await getWasPreviouslyOnline();
     if (isOnline && !wasPreviouslyOnline) {
-        openTab(this.username);
+        console.log("Is now online. Opening in a new tab");
+        openTab(username);
     }
-    await browser.storage.local.set("previously_online", isOnline);
+    await browser.storage.local.set({previously_online: isOnline});
+}
+
+async function init() {
+    await browser.storage.local.set({previously_online: false});
+    configChanged();
 }
 
 browser.alarms.onAlarm.addListener((alarm) => {
@@ -51,8 +69,16 @@ browser.alarms.onAlarm.addListener((alarm) => {
 
 browser.runtime.onMessage.addListener(function(request, sender) {
     if (request.type === "config_changed") {
+        console.log("Config changed");
         return configChanged();
     }
     return false;
 });
 
+browser.runtime.onStartup.addListener(() => {
+    init();
+})
+
+browser.runtime.onInstalled.addListener(() => {
+    browser.tabs.create({url: browser.runtime.getURL('src/popup.html')})
+})
--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,17 @@
 (function() {
     let input = document.getElementById("id_enable-ghax");
+    const isInPopup = function() {
+        return (typeof chrome != undefined && chrome.extension) ?
+            chrome.extension.getViews({ type: "popup" }).length > 0 : null;
+    }
     input.addEventListener("change", async () => {
         let checked = input.checked;
         chrome.storage.local.set({"ghax_enabled": checked});
         if (checked) {
             let promise = browser.permissions.request({origins: ["https://decapi.me/twitch/uptime/*"]});
-            window.close()
+            if (isInPopup() && !(await browser.permissions.contains({origins: ["https://decapi.me/twitch/uptime/*"]}))) {
+                window.close();
+            }
             await promise;
         }
         chrome.runtime.sendMessage(null, {type: "config_changed"})
@@ -14,7 +20,7 @@
 
     let intervalInput = document.getElementById("id_interval");
     intervalInput.addEventListener("input", () => {
-        chrome.storage.local.set({"interval": intervalInput.value});
+        chrome.storage.local.set({"interval": parseInt(intervalInput.value)});
         chrome.runtime.sendMessage(null, {type: "config_changed"})
     });
     chrome.storage.local.get("interval", (x) => intervalInput.value = x.interval);
